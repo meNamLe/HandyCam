90458d3257ca82994ea2208044e97815eec750a3
NamLe <vnamle1@gmail.com>
2018-03-18 04:40:27 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,31 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { CameraPreview } from '@ionic-native/camera-preview';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+  constructor(statusBar: StatusBar, splashScreen: SplashScreen, private cameraPreview: CameraPreview, private platform: Platform) {
+    this.platform.ready().then(() => {
+
+
       statusBar.styleDefault();
       splashScreen.hide();
-    });
+
+      let options = {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: this.cameraPreview.CAMERA_DIRECTION.BACK,
+        toBack: true,
+        tapPhoto: false,
+        previewDrag: false
+      };
+      this.cameraPreview.startCamera(options);
+    })
   }
 }
-
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import * as Clarifai from 'clarifai';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  app = new Clarifai.App({
+    apiKey: 'd1f89510500542898b0f12b699852ba1'
+  });
+  err: string = "buttface";
+  success: number = 1738;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private zone: NgZone) {}
 
+  start(){
+
+  }
+  
+  refresh(){
+    window['location'].reload();
+  }
+
+
+  ////////////////////////
+  ///////* Clarifai */////
+  ////////////////////////
+
+  setupInputs(){
+    this.app.inputs.create({
+      url: "https://static.pexels.com/photos/59523/pexels-photo-59523.jpeg",
+      concepts: [
+        {
+          id: "fiddo",
+          value: true
+        }
+      ]
+    });
+  }
+
+  createMachine(){
+    this.app.models.create(
+      "pets",
+      [
+        { "id": "fiddo" }
+      ]
+    ).then(
+      function(response) {
+        console.log('success')
+        console.log(response);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  }
+
+  trainMachine(){
+    this.app.models.train("pets").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  }
+
+  machinePredict(){
+    this.app.models.predict("pets", ["https://samples.clarifai.com/puppy.jpeg"]).then(
+    function(response) {
+      //outputs 0 data concepts 0 value
+      console.log(response.outputs[0].data.concepts[0].value);
+    },
+    function(err) {
+      console.log(err);
+    }
+  );
+  }
+
+  machinePredict2(){
+
+    this.app.models.predict("pets", ["https://static.pexels.com/photos/59523/pexels-photo-59523.jpeg"]).then(
+
+      (response) => {
+        console.log(this.err);
+        console.log(response.outputs[0].data.concepts[0].value);
+        this.success = response.outputs[0].data.concepts[0].value;
+      },
+      (err) => {
+        console.log(err.data.status.details);
+        this.err = err.data.status.details;
+      }
+    )
+
+  }
+  
+  reload(){
+    console.log(this.success)
   }
 
 }
