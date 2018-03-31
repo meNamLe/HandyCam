c3b6aab8ec5840ee9a832be4f8c5287ff2d2f2fa
NamLe <vnamle1@gmail.com>
2018-03-30 23:31:02 -0400

    less go

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -10,7 +10,7 @@ import * as Clarifai from 'clarifai';
 })
 export class CameraPage {
   app = new Clarifai.App({
-    apiKey: 'da60c697e9b7404f9192684ffca0c146'
+    apiKey: 'a1086fa6198d49d98d989069d3cc724d'
   });
   
   base64Image: string;
@@ -23,8 +23,8 @@ export class CameraPage {
     quality: 85
   }
 
-  word = ["k","i","t","e"];
-  picArr = ['','','','','','','','','',''];
+  word = ["c","a","b","b","a","g","e"];
+  picArr = ['','','','','','',''];
   picArrNum = 0;
   results: string = '';
   hit = 0;
@@ -72,54 +72,65 @@ export class CameraPage {
   //cameraPreview capture picture function
   async takePicture(){
     // take a picture
-    await this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+    /* await this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
       this.picArr[this.picArrNum] = ('data:image/jpeg;base64,' + imageData);
       this.picArrNum++;
     }, (err) => {
       console.log(err);
-    });
+    }); */
+    console.log('picture captured');
   }
 
    //clarifai predict function
-  predict = (picArr) => {
-      let imageData = picArr.map((base64) => {return base64.replace(/^data:image\/(.*);base64,/, '')});
-      this.app.models.predict("reddy", imageData ).then(
+  predict = async () => {
+      let imageData = this.picArr.map((base64) => {return base64.replace(/^data:image\/(.*);base64,/, '')});
+      //let imageData = picArr;
+      await this.app.models.predict("read-my-hands", imageData ).then(
   
           (response) => {
                   console.log('predit');
-                  for(let i = 0; i< 4; i++){
+                  console.log(response);
+                  for(let i = 0; i< 7; i++){
                     if(response.outputs[i].data.concepts[0].name == this.word[i]){
                       this.results = this.results + response.outputs[i].data.concepts[0].name;
                       console.log(response.outputs[i].data.concepts[0].name)
                       console.log(this.results);
                     }
                   }
-                  this.hit = this.hit + 1;
-
           }, 
           (err) => {
                   console.log(err);
 
           }
       )
+      this.hit = this.hit + 1;
+      this.statsSwitch();
     }
 
-    startPictures(){
-      let testArr = ['https://i.imgur.com/zkYJgbU.jpg', 'https://i.imgur.com/JxHBIJn.jpg','https://i.imgur.com/xocAXh0.jpg','https://i.imgur.com/Po7idoZ.jpg'];    
+/*     startPictures(){
+      let testArr = ['https://i.imgur.com/FdheT2t.jpg', 'https://i.imgur.com/vKCXfnN.jpg','https://i.imgur.com/Yy7oysN.jpg','https://i.imgur.com/Yy7oysN.jpg','https://i.imgur.com/vKCXfnN.jpg','https://i.imgur.com/ufdOLSQ.jpg','https://i.imgur.com/OASiZwH.jpg'];    
+
 
       setTimeout(() => this.takePicture() , 1000);
       setTimeout(() => this.takePicture() , 2000);
-      setTimeout(() => this.takePicture() , 3000);
-      setTimeout(() => this.takePicture() , 4000);
-      setTimeout(() => this.predict(this.picArr), 5000);
- /*      setTimeout(() => this.takePicture() , 5000);
+      setTimeout(() => this.predict(testArr), 3000);
+      setTimeout(() => this.takePicture() , 5000);
       setTimeout(() => this.takePicture() , 6000);
       setTimeout(() => this.takePicture() , 7000);
       setTimeout(() => this.takePicture() , 8000);
       setTimeout(() => this.takePicture() , 9000);
-      setTimeout(() => this.takePicture() , 10000); */
-/*       setTimeout(() => this.predict(testArr) , 11000);
- */    }
+      setTimeout(() => this.takePicture() , 10000);
+     } */
+     startPictures(){
+      setTimeout(() => this.takePicture() , 1000);
+      setTimeout(() => this.takePicture() , 2000);
+      setTimeout(() => this.takePicture() , 3000);
+      setTimeout(() => this.takePicture() , 4000);
+      setTimeout(() => this.takePicture() , 5000);
+      setTimeout(() => this.takePicture() , 6000);
+      setTimeout(() => this.takePicture() , 7000);
+      setTimeout(() => this.predict() , 8000);
+    }
 } 
 /*   
       if(this.difficulty == 1){
